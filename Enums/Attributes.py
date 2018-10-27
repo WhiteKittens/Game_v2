901ec9875a604326cf789fb6dee65ee0e26618c9
quinten <quinten.possemiers@gmail.com>
2018-10-27 16:30:50 +0200

    Init

--- Enums/Attributes.py
+++ Enums/Attributes.py
@@ -2,19 +2,25 @@ from enum import Enum
 
 
 class Attributes(Enum):
-    critical_strike_chance = 0, 1
-    critical_strike_multiplier = 1, 1
-    global_damage_multiplier = 2, 1
-    life_steal = 3, 0.2
-    energy_regen = 4, 1
-    max_energy = 5, 1
-    percent_life = 6, 1
-    armor = 7, 1
-    evasion_rating = 8, 1
-    poison = 9, 1
-    poison_resist = 10, 1
-    elemental_conversion = 11, 1
-    elemental_resistance = 12, 1
-    elemental_bonus = 13, 1
-    armor_penetration = 14, 1
-    life = 15, 45
+    """
+    Name
+    0) ID
+    1) True weapon only     False armour only     None both
+    2) Base value
+    """
+    critical_strike_chance = 0, True, 1
+    critical_strike_multiplier = 1, True, 1
+    global_damage_multiplier = 2, False, 1
+    life_steal = 3, True, 1
+    energy_regen = 4, None, 1
+    max_energy = 5, None, 1
+    percent_life = 6, False, 1
+    armor = 7, False, 1
+    evasion_rating = 8, False, 1
+    poison = 9, True, 1
+    poison_resist = 10, False, 1
+    elemental_conversion = 11, True, 1
+    elemental_resistance = 12, False, 1
+    elemental_bonus = 13, True, 1
+    armor_penetration = 14, True, 1
+    life = 15, False, 1
